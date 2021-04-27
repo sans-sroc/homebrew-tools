ba4ecabefa40d1be5af294585580a533d1bdd37a
Erik Kristensen <erik@erikkristensen.com>
2021-04-26 18:33:39 -0600

    switch to old archive format so renovate works

--- Formula/integrity.rb
+++ Formula/integrity.rb
@@ -1,7 +1,7 @@
 class Integrity < Formula
   desc "Validates files in a given directory"
   homepage "https://github.com/sans-sroc/integrity"
-  url "https://github.com/sans-sroc/integrity/archive/refs/tags/1.0.6.tar.gz"
+  url "https://github.com/sans-sroc/integrity/archive/1.0.6.tar.gz"
   sha256 "322cefb950eba8fdf784e33429b4cfeb30f6b1d1c81311d968b91f168dfd8c20"
   license "MIT"
 
