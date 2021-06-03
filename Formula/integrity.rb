3a33c8f903713b5d3cde694b04b7c7787dc67dae
Erik Kristensen <erik@erikkristensen.com>
2021-06-03 18:31:21 +0000

    integrity: add 3.0.2 bottle.

--- Formula/integrity.rb
+++ Formula/integrity.rb
@@ -5,6 +5,12 @@ class Integrity < Formula
   sha256 "aefaadf4db418af76d25e0f6312962836d0be0cdd08a836f5d9b0fa52b14087e"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/sans-sroc/homebrew-tools/releases/download/integrity-3.0.2"
+    sha256 cellar: :any_skip_relocation, catalina:     "5ca1c62b5301fbe530ae077d6caa015080a3db475e55373174e8a85153433e3d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "6e200a1c5fffe185b623ac118f4b6019d1bd31469c380c4d26a1e0fbee8390f0"
+  end
+
   depends_on "go" => :build
 
   def install
